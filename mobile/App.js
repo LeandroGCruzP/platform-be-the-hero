f7ebd89be52b5ec7629027376cfb89d31d7f840f
Leandro Cruz <leandro.programmer@hotmail.com>
2020-03-27 11:39:06 -0300

    Mobile da App

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Router from './src/routes';
+
+export default function App() {
+  return (
+    <Router />
+  );
+}
